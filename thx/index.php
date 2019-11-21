23ffd3d15c0af777ba2bc5da6e67eee960335ad8
dimondc <57995221+dimondc@users.noreply.github.com>
2019-11-21 10:23:34 +0300

    Update index.php

--- thx/index.php
+++ thx/index.php
@@ -2,6 +2,6 @@
 <head>
 </head>
 <body>
-<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=767594573666487&ev=Lead&noscript=1"/>
+<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=2404597996459912&ev=Lead&noscript=1"/>
 </body>
 </html>
